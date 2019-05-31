5f8f97215af48921753048ea36b5529a0ff470fa
Reinaldo Ordi DEPP B2 <reinaldodos@gmail.com>
2019-05-31 12:08:24 +0200

    carto France
    carto France

--- Europeennes 2019/Carto/Carto.R
+++ Europeennes 2019/Carto/Carto.R
@@ -1,10 +1,11 @@
 pacman::p_load(tidyverse, data.table, rio, sf)
 
-opendata = "/Users/reinaldodossantos/Downloads/communes-20190101-shp/"
+opendata = "Europeennes 2019/Carto/Communes/"
 
 input =
-  opendata %>% list.files(pattern = "shp", full.names = T) %>%
-  st_read(dsn = .)
+  opendata %>% list.files(pattern = "\\.shp", full.names = T) %>%
+  st_read(dsn = .) %>%
+  st_transform(crs = 2154)
 
 BASE = src_sqlite(path = "Europeennes 2019/Soiree_electorale/BASE", create = FALSE)
 
@@ -16,12 +17,15 @@ Results =
            str_sub(start = -3)) %>%
   mutate(insee = str_c(Code.du.departement, Code.de.la.commune)) %>%
   select(insee, contains("departement"), Bloc, Voix) %>%
-  group_by(insee) %>% mutate(Inscrits = sum(Voix)) %>%
-  mutate(Voix = Voix / Inscrits * 100) %>% ungroup %>%
-  spread(key = Bloc, value = Voix)
+  group_by(insee) %>% mutate(Inscrits = sum(Voix)) %>% ungroup
+  # mutate(Voix = Voix / Inscrits * 100) %>% ungroup %>%
+
+Blocs = Results %>% distinct(Bloc) %>% pull
 
 Results =
-  Results %>% filter(Code.du.departement=="33")
+  Results %>%
+  # filter(Code.du.departement == "78") %>%
+  spread(key = Bloc, value = Voix)
 
 # Choropleth -------------------------------------------------------------
 pacman::p_load(cartography)
@@ -30,66 +34,75 @@ koropless =
   inner_join(x = input, by = "insee")
 
 koropless %>%
+  mutate_at(.vars = Blocs, .funs = ~100*./Inscrits) %>%
   choroLayer(var = "EXD",
              nclass = 5,
              method = "sd")
 
-breaks = classInt::classIntervals(var = koropless$EXD, n = 5, style = "sd")
+pacman::p_load(classInt)
 
 GROUP_KOR =
   koropless %>%
-  mutate(Groupe = cut(EXD, breaks = breaks$brks, include.lowest = T)) %>%
+  mutate_at(.vars = Blocs, .funs = ~100*./Inscrits) %>%
+  mutate(Groupe = cut(
+    EXD,
+    breaks = classIntervals(var = EXD, n = 5, style = "sd")$brks,
+    include.lowest = T
+  )) %>%
   group_by(Groupe) %>%
   summarize(EXD = sum(EXD * Inscrits) / sum(Inscrits),
             Inscrits = sum(Inscrits))
 
 GROUP_KOR %>%
-  choroLayer(var = "EXD", breaks = breaks$brks)
+  choroLayer(var = "EXD")
+
+# Lissage -----------------------------------------------------------------
+base_temp =
+  koropless %>% st_centroid() %>% st_coordinates() %>% data.table() %>%
+  cbind.data.frame(koropless) %>%
+  rename(x = X, y = Y)
+
+pacman::p_load(btb, spatstat)
+
+base.ppp = spatstat::ppp(base_temp$x, base_temp$y,
+                         c(min(base_temp$x), max(base_temp$x)),
+                         c(min(base_temp$y), max(base_temp$y)))
+
+SIGMA = bw.diggle(X = base.ppp)
+
+Liss =
+  base_temp %>%
+  keep(.p = is.numeric) %>%
+  kernelSmoothing(iCellSize = SIGMA,
+                  iBandwidth = SIGMA * 2,
+                  sEPSG = 2154)
+
+Liss %>%
+  mutate_at(.vars = Blocs, .funs = ~100*./Inscrits) %>%
+  choroLayer(var = "EXD", nclass = 5, method = "sd")
 
 # Anamorphose -------------------------------------------------------------
 pacman::p_load(cartogram, lwgeom)
 
 anamorf =
-  GROUP_KOR %>%
+  Liss %>%
   st_cast(to = "MULTIPOLYGON") %>%
   cartogram_cont(weight = "Inscrits",
-                 itermax = 2)
+                 itermax = 12)
 
 anamorf %>%
-  choroLayer(var = "EXD",
-             breaks = breaks$brks)
+  mutate_at(.vars = Blocs, .funs = ~ 100 * . / Inscrits) %>%
+  choroLayer(var = "EXD", nclass = 5, method = "sd")
 
 # Carroyage ---------------------------------------------------------------
-KARO =
-  koropless %>%
-  getGridLayer(cellsize = .025 ^ 2,
-               type = "hexagonal",
-               var = "EXD")
-
-KARO %>%
-  choroLayer(var = "EXD",
-             breaks = breaks$brks)
-
-KARO %>%
-  st_join(koropless %>% select(Inscrits, geometry)) %>%
-  mutate(Groupe = cut(EXD, breaks = breaks$brks, include.lowest = T)) %>%
+anamorf %>%
+  mutate_at(.vars = Blocs, .funs = ~ 100 * . / Inscrits) %>%
+  mutate(Groupe = cut(
+    x = EXD,
+    breaks = classIntervals(var = EXD, n = 5, style = "sd")$brks,
+    include.lowest = T
+  )) %>%
   group_by(Groupe) %>%
   summarize(EXD = sum(EXD * Inscrits) / sum(Inscrits),
             Inscrits = sum(Inscrits)) %>%
-  choroLayer(var = "EXD", breaks = breaks$brks)
-
-# Lissage -----------------------------------------------------------------
-base_temp =
-  koropless %>% st_centroid() %>% st_coordinates() %>% data.table() %>%
-  cbind.data.frame(koropless)
-
-
-base.ppp = spatstat::ppp(base_temp$X, base_temp$Y,
-                         c(min(base_temp$X), max(base_temp$X)),
-                         c(min(base_temp$Y), max(base_temp$Y)))
-
-DENS =
-  spatstat::density.ppp (base.ppp, sigma = .025,
-                         weights = base_temp$EXD)
-
-DENS %>% plot()
+  choroLayer(var = "EXD")
