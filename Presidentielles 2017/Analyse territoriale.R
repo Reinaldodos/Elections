b4a19e62f7d02895394d69f56bc19ba631562ec5
Reinaldo Mac maison <reinaldodos@gmail.com>
2020-08-16 09:48:09 +0200

    retouche Pdt 2017

--- Presidentielles 2017/Analyse territoriale.R
+++ Presidentielles 2017/Analyse territoriale.R
@@ -5,11 +5,14 @@ BASE = src_sqlite(path = "Presidentielles 2017/BASE", create = F)
 Resultats = tbl(src = BASE, "Resultats")
 Chiffres = tbl(src = BASE, "Chiffres_globaux")
 
-MAT=
+MAT =
   Resultats %>% as.data.table() %>%
   spread(key = Listes, value = Voix, fill = 0) %>%
-  inner_join(y = Chiffres %>% select(rowid, Abstentions, Blancs, Nuls),
-             by = "rowid", copy=T)
+  inner_join(
+    y = Chiffres %>% select(rowid, Abstentions, Blancs, Nuls),
+    by = "rowid",
+    copy = T
+  )
 
 KOR =
   MAT %>% select(-rowid) %>%
@@ -19,17 +22,24 @@ KOR %>% ggcorrplot::ggcorrplot(hc.order = T)
 
 require(psych)
 KOR %>% fa.parallel(n.obs = nrow(MAT), fa = "both")
+KOR %>% nfactors(rotate = "oblimin", n.obs = nrow(MAT))
 
 KOR %>% psych::pca(nfactors = 2, n.obs = nrow(MAT)) %>% fa.diagram()
 
-KOR %>%
-  psych::fa(nfactors = 5, n.obs = nrow(MAT),
-            rotate = "oblimin") %>%
-  fa.diagram(simple = F, cut = .25)
+FA =
+  KOR %>%
+  psych::fa(nfactors = 3,
+            n.obs = nrow(MAT),
+            rotate = "oblimin")
+
+FA %>% fa.diagram(simple = F, cut = .3)
+
+FA$loadings
 
 hc = KOR %>% dist() %>% hclust(method = "ward.D")
+
+require(ggdendro)
 hc %>% ggdendrogram(rotate = T)
 
 KOR %>%
   psych::omega(nfactors = 5, n.obs = nrow(MAT))
-
