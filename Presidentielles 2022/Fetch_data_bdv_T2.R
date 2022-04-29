19abb6afa74cb3e92270d1c4adcd5dd49e304d52
Reinaldo Dos Santos <reinalddos@gmail.com>
2022-04-29 08:21:26 +0200

    data bdv pour 2022 deux tours

--- Presidentielles 2022/Fetch_data_bdv_T2.R
+++ Presidentielles 2022/Fetch_data_bdv_T2.R
@@ -3,7 +3,7 @@ require(magrittr)
 require(data.table)
 
 input =
-  "~/Téléchargements/resultats-par-niveau-burvot-t1-france-entiere.xlsx" %>%
+  "~/Téléchargements/resultats-par-niveau-burvot-t2-france-entiere.xlsx" %>%
   readxl::read_excel()
 
 input %<>% janitor::clean_names()
@@ -14,8 +14,9 @@ CLINNE <- function(data) {
   names(data) = c("rowid", "candidat", "voix")
   return(data)
 }
+input %>% sample_n(1) %>% as.list()
 Scores =
-  map(.x = 7*0:11, .f = ~input[,c(1, 25 + ., 27 + .)]) %>%
+  map(.x = 7*0:1, .f = ~input[,c(1, 25 + ., 27 + .)]) %>%
   map(.f = CLINNE) %>%
   rbindlist()
 Bureaux = input[, 1:8]
@@ -26,11 +27,11 @@ Abstention =
                names_to = "candidat",
                values_to = "voix")
 
-output_T1 =
+output_T2 =
   bind_rows(Scores, Abstention) %>%
   inner_join(x = Bureaux, by = "rowid")
 
-output_T1 %<>%
+output_T2 %<>%
   group_by(rowid) %>%
   mutate(score = voix/sum(voix)) %>%
   ungroup()
